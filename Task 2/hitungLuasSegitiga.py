d17102afbe6eaa57f8755061bcb6c9f1ebe0ce80
hanz-fr <raihanf787@gmail.com>
2024-09-21 18:07:28 +0700

    remove zip file

---
+++ Task 2/hitungLuasSegitiga.py
@@ -0,0 +1,5 @@
+alas = float(input("Input alas : "))
+tinggi = float(input("Input tinggi : "))
+luas = float(0.5*alas*tinggi)
+
+print(f"Luas segitiga adalah {luas}")
---
+++ Task 2/simpleCalculator.py
@@ -0,0 +1,6 @@
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+sum = a+b+c
+
+print(f"Hasil dari penjumlahan {a} + {b} + {c} = {sum}")
